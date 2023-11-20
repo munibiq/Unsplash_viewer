24fb9ce09c8775d01b969e5c8a5e671b111aa2b3
munibiq <munibiqbal@arcor.de>
2023-11-20 15:05:33 +0100

    fixed repetition of createElement

--- script.js
+++ script.js
@@ -37,8 +37,6 @@ function displayPhotos() {
     photosArray.forEach((photo) => {
         // creating a to link unsplash
         const item = document.createElement('a');
-        // item.setAttribute('href', photo.links.html);
-        // item.setAttribute('target', '_blank');
         setAttributes(item, {
             href: photo.links.html,
             target: '_blank',
@@ -46,9 +44,6 @@ function displayPhotos() {
         })
         // Create img for photo
         const img = document.createElement('img');
-        // img.setAttribute('src', photo.urls.regular);
-        // img.setAttribute('alt', photo.alt_description);
-        // img.setAttribute('title', photo.alt_description);
         setAttributes(img, {
             src: photo.urls.regular,
             alt: photo.alt_description,
